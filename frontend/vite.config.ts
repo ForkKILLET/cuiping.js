1d563a016608a99545da403e01c9ed03ace598e9
ForkΨKILLET <fork_killet@qq.com>
2022-11-23 20:30:07 +0800

    fix(frontend): fix base URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+	base: './',
 	build: {
-		outDir: '../docs'
+		outDir: '../docs',
 	},
 	plugins: [ vue() ]
 })
