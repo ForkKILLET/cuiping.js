a786fb389cb5c6cb8917660d8a6636615e5576d4
ForkΨKILLET <fork_killet@qq.com>
2022-11-23 20:24:06 +0800

    feat(frontend): add examples and about

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,110 @@ import { ref } from 'vue'
 import Cuiping from './components/Cuiping.vue'
 
 const molecule = ref<string | void>()
+
+const examples = [
+    'C[--||H]',
+    'C[H\\/,=-C[\\/H]]',
+    'S[-|OH,O-|]',
+    'C[H-,||H,-C[=|O,-O[-H]]]'
+]
 </script>
 
 <template>
-    <p>Cuiping.js</p>
+    <header>
+        <h1>Cuiping.js</h1>
 
-    <input v-model="molecule" />
+        <p><input v-model="molecule" placeholder="Enter Cuiping formula" /></p>
 
-    <Cuiping :molecule="molecule" />
+        <p>The structure of <code>"{{ molecule }}"</code> is:</p>
+        <Cuiping :molecule="molecule" />
+    </header>
+
+    <article>
+        <h2>Examples</h2>
+        <div class="examples">
+            <div v-for="mol in examples">
+                <code>{{ mol }}</code>
+                <Cuiping :molecule="mol" :key="mol" />
+            </div>
+        </div>
+
+        <h2>About</h2>
+        GitHub: <a href="//github.com/ForkKILLET/cuiping.js">ForkKILLET/cuiping.js</a> <br />
+        npm: <a href="//www.npmjs.com/package/cuiping">cuiping</a>
+    </article>
 </template>
+
+<style scoped>
+header {
+    padding-top: 10vh;
+    text-align: center;
+    background-color: #1f1a1a;
+    color: white;
+}
+
+article {
+    margin: 0;
+    padding: 2vw 5vw;
+    background-color: #e6f0e8;
+}
+
+
+h1 {
+    color: #1cd91c;
+    font-size: 3em;
+}
+
+h2 {
+    position: relative;
+    margin-left: 20vw;
+}
+
+h2::before {
+    position: absolute;
+    display: block;
+    content: '';
+    left: -20vw;
+    top: .4em;
+    width: calc(20vw - .5em);
+    height: .1em;
+    background-color: black;
+}
+
+input {
+    font-size: 1.6em;
+    outline: none;
+    border: .1em solid black;
+    border-radius: .3em;
+    padding: .1em;
+}
+
+code {
+    color: #1cd91c;
+}
+
+article code {
+    display: block;
+    background-color: black;
+    padding: .1em .3em;
+    border-radius: .4em;
+    margin-bottom: 1em;
+}
+
+.examples {
+    display: flex;
+}
+
+.examples > div {
+    display: inline-block;
+    padding: 1em;
+}
+
+.examples > div:nth-child(2n + 1) {
+    background-color: #e0e0e0;
+}
+
+:deep(svg) {
+    background-color: white;
+}
+</style>
--- frontend/src/components/Cuiping.vue
+++ frontend/src/components/Cuiping.vue
@@ -30,20 +30,83 @@ const svg = computed(() => {
     const svg = renderSVG(layout)
     return svg
 })
+
+function downloadSvg() {
+    const blob = new Blob([svg.value])
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.download = 'mol.svg'
+    a.href = url
+    a.click()
+    URL.revokeObjectURL(url)
+}
+
+const isCopying = ref<boolean>(false)
+
+function copyFormula() {
+    if (isCopying.value) return
+    isCopying.value = true
+    navigator.clipboard
+        .writeText(props.molecule ?? '')
+        .finally(() => {
+            isCopying.value = false
+        })
+}
 </script>
 
 <template>
-    <div>
-        <p>The structure of <code>{{ molecule }}</code> is:</p>
-        <div v-html="svg" :class="{ error }"></div>
+    <div class="root">
+        <div v-html="svg" class="container" :class="{ error }"></div>
+        <div class="toolbar">
+            <div class="toolbar-inner">
+                <button @click="downloadSvg">Download</button>
+                <button @click="copyFormula">{{ isCopying ? 'Copying...' : 'Cuiping' }}</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-code {
-    color: forestgreen;
-}
 .error {
     color: red;
+}
+
+.toolbar {
+    position: absolute;
+    display: none;
+    top: 0;
+    left: 100%;
+    height: 100%;
+}
+
+.toolbar-inner {
+    display: inline-flex;
+    margin: 0 .5em;
+    background-color: white;
+    border-radius: .4em;
+    box-shadow: 0 0 0.1em 0.1em #dadada;
+    overflow: hidden;
+}
+
+.toolbar button {
+    background: none;
+    border: none;
+    outline: none;
+    color: #1f1a1a;
+    font-size: .8em;
+    transition: .5s background-color;
+}
+
+.toolbar button:hover {
+    background-color: #dadada;
+}
+
+.root:hover .toolbar {
+    display: block;
+}
+
+.root {
+    position: relative;
+    display: inline-block;
 }
 </style>
