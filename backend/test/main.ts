8307854b35e17eaec42c2874c83bdc0d75462395
ForkΨKILLET <fork_killet@qq.com>
2022-11-27 18:18:21 +0800

    fix(test): fix test loop

--- backend/test/main.ts
+++ backend/test/main.ts
@@ -7,22 +7,21 @@ import { renderSVG } from '../core/render.js'
 export function testChem(input: string) {
 	const parser = new ChemParser(input)
 
-	try {
-		const chem = parser.parse()
-		Debug.O(chem)
-
-		const chemEx = expandAggregateBonds(chem, 0)
-		Debug.O(chemEx)
-
-		const layout = locate(chemEx)
-		Debug.O(layout)
-
-		const svg = renderSVG(layout)
-		Debug.O(svg)
-	}
-	catch (err) {
+	const chem = parser.parse((err) => {
 		Debug.E(
 			Debug.on ? (err as Error).stack : err
 		)
-	}
+		return true
+	})
+	if (chem) Debug.O(chem)
+	else return
+
+	const chemEx = expandAggregateBonds(chem, 0)
+	Debug.O(chemEx)
+
+	const layout = locate(chemEx)
+	Debug.O(layout)
+
+	const svg = renderSVG(layout)
+	Debug.O(svg)
 }
