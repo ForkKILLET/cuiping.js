a8de43c3b2e225102ce0f3d9647e3638037095af
ForkΨKILLET <fork_killet@qq.com>
2022-12-19 21:26:10 +0800

    fix(frontend): i18n warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 	base: './',
+	resolve: {
+		alias: [
+			{
+				find: 'vue-i18n',
+				replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+			}
+		]
+	},
 	build: {
 		outDir: '../dist',
 		emptyOutDir: true
 	},
-	plugins: [ vue() ]
+	plugins: [vue()]
 })
