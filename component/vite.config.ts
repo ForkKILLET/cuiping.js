423c261efd05a6e4f715a4b92b694b5aaa707a9e
ForkΨKILLET <fork_killet@qq.com>
2023-01-02 12:11:20 +0800

    chore: config vite HMR

--- component/vite.config.ts
+++ component/vite.config.ts
@@ -2,23 +2,17 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [ vue() ],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/main.ts'),
             name: 'CuipingComponent',
-            // the proper extensions will be added
             fileName: 'cuiping-component'
         },
         rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['vue'],
+            external: [ 'vue' ],
             output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
                 globals: {
                     vue: 'Vue'
                 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Alias } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-	base: './',
-	resolve: {
-		alias: [
-			{
-				find: 'vue-i18n',
-				replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
-			}
-		]
-	},
-	build: {
-		outDir: '../dist',
-		emptyOutDir: true
-	},
-	plugins: [vue()]
+export default defineConfig(({ command }) => {
+	const alias: Alias[] = [
+		{
+			find: 'vue-i18n',
+			replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+		}
+	]
+
+	if (command === 'serve') alias.push({
+		find: /cuiping-component$/,
+		replacement: 'cuiping-component/lib/main.ts'
+	}, {
+		find: /cuiping$/,
+		replacement: 'cuiping/core/index.ts'
+	})
+
+	return {
+		plugins: [
+			vue()
+		],
+		base: './',
+		resolve: {
+			alias
+		},
+		build: {
+			outDir: '../dist',
+			emptyOutDir: true
+		}
+	}
 })
