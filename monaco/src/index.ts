45625906eadd257305a0a56708b761f9de339ba6
ForkΨKILLET <fork_killet@qq.com>
2023-01-07 00:23:36 +0800

    fix(monaco): kill the worker error

--- monaco/src/index.ts
+++ monaco/src/index.ts
@@ -148,5 +148,8 @@ export const getMonacoForCuiping = (monaco: typeof Monaco) => {
         }
     })
 
+    if (MonacoEnvironment) MonacoEnvironment.getWorkerUrl = () => 'data:application/javascript;base64,'
+        + btoa(`console.log("Mocano, I DON'T NEED ANY WORKERS, YOU KNOW?")`)
+
     return monaco
 }
