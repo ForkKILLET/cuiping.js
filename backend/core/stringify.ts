b6528ea5694b12b76cd6962d196ab6440f6fc915
ForkΨKILLET <fork_killet@qq.com>
2023-01-18 16:47:08 +0800

    feat(backend/stringify): remove unnecessary `[loop]`

--- backend/core/stringify.ts
+++ backend/core/stringify.ts
@@ -1,6 +1,6 @@
-import type { Struct } from './index'
+import type { Bond, Struct } from './index'
 
-export function stringifyStruct(struct: Struct, memory = new Set<Struct>(), depth = 0) {
+export function stringifyStruct(struct: Struct, memory: Struct[] = [], depth = 0) {
     const headStr = `[${struct.S}] ` + (
         struct.S === 'chem'
             ? struct.node.t.B.map(x => x.s).join('')
@@ -8,19 +8,26 @@ export function stringifyStruct(struct: Struct, memory = new Set<Struct>(), dept
                 ? '&' + struct.node.names.join(',')
                 : ''
     )
-    if (memory.has(struct)) return headStr + ' [loop]\n'
-    else memory.add(struct)
-    const childrenStr: string = struct.children
-        .map(({ n, d }) => '->  '.repeat(depth + 1)
-            + d.join(',').padEnd(3) + ' '
-            + stringifyStruct(n, memory, depth + 1)
-        )
-        .join('\n')
-    return headStr + '\n' + childrenStr
+    if (memory.includes(struct)) return headStr + ' [loop]\n'
+    else memory = [ ...memory, struct ]
+
+    const next = (bonds: Bond[], symbol: string): string => {
+        const str = bonds
+            .map(({ n, d }) => symbol.padStart(4 * (depth + 1))
+                + d.join(',').padEnd(3) + ' '
+                + stringifyStruct(n, memory, depth + 1)
+            )
+            .join('\n')
+        return str
+    }
+    const childrenStr = next(struct.children, '->  ')
+    const parentStr = next(struct.parents, '<-  ')
+
+    return headStr + '\n' + childrenStr + parentStr
 }
 
 export function printStruct(struct: Struct) {
-    console.log(stringifyStruct(struct))
+    console.log('%o\n%s', struct, stringifyStruct(struct))
 }
 
 export function wrapStructString(struct: Struct) {
