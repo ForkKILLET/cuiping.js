f6d2e484fd1b2d65194693ac5c05489f379b0af3
汪心禾 <wangxinhe06@gmail.com>
2023-01-21 19:15:11 +0800

    chore(lint): lint Vite config files with ESLint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,47 +1,47 @@
-import { defineConfig, Alias, Plugin } from 'vite'
+import { defineConfig } from 'vite'
+import type { Alias, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import monacoEditor from 'vite-plugin-monaco-editor'
 
 export default defineConfig(({ command }) => {
-	const alias: Alias[] = [
-		{
-			find: 'vue-i18n',
-			replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
-		}
-	]
+    const alias: Alias[] = [
+        {
+            find: 'vue-i18n',
+            replacement: 'vue-i18n/dist/vue-i18n.cjs.js'
+        }
+    ]
 
-	if (command === 'serve') alias.push({
-		find: /cuiping-component$/,
-		replacement: 'cuiping-component/lib/main.ts'
-	}, {
-		find: /cuiping$/,
-		replacement: 'cuiping/core/index.ts'
-	}, {
-		find: /cuiping-monaco$/,
-		replacement: 'cuiping-monaco/src/index.ts'
-	})
+    if (command === 'serve') alias.push({
+        find: /cuiping-component$/,
+        replacement: 'cuiping-component/lib/main.ts'
+    }, {
+        find: /cuiping$/,
+        replacement: 'cuiping/core/index.ts'
+    }, {
+        find: /cuiping-monaco$/,
+        replacement: 'cuiping-monaco/src/index.ts'
+    })
 
-	return {
-		plugins: [
-			vue(),
-			// @ts-ignore
-			monacoEditor.default({
-				languageWorkers: [],
-				customWorkers: [
-					{
-						label: 'editorWorkerService',
-						entry: 'monaco-editor/esm/vs/editor/editor.worker',
-					}
-				]
-			})
-		],
-		base: './',
-		resolve: {
-			alias
-		},
-		build: {
-			outDir: '../dist',
-			emptyOutDir: true
-		}
-	}
+    return {
+        plugins: [
+            vue(),
+            monacoEditor({
+                languageWorkers: [],
+                customWorkers: [
+                    {
+                        label: 'editorWorkerService',
+                        entry: 'monaco-editor/esm/vs/editor/editor.worker'
+                    }
+                ]
+            }) as Plugin
+        ],
+        base: './',
+        resolve: {
+            alias
+        },
+        build: {
+            outDir: '../dist',
+            emptyOutDir: true
+        }
+    }
 })
